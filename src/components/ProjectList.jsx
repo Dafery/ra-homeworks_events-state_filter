3ecfad2d16fb4b7edc1dd1de50831ce03961eda9
Maria Abramovich <mashkala2@gmail.com>
2023-06-29 03:32:58 +0300

    first commit

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  { v4 as uuidv4 } from 'uuid';
+
+export const ProjectList = ({filteredProjects}) => {
+  
+  return (
+    <ul className='project-list'>
+      {filteredProjects.map((project) => {            
+        return (
+          <li className='project-item' key={uuidv4()}>
+            <img className='project-img' src={project.img} />
+          </li>
+        );
+      })}
+    </ul>
+  )
+}
